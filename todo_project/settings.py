67e5f7a3638fd561e2d0a26952c880021f12ebfe
Ajay Nishad <ajaynishad4123@gmail.com>
2026-01-10 22:58:28 +0530

    Read SECRET_KEY from environment variables

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5eez0p9aa0&wu6^uwj3j-*e(sgjigm8jr2hdnm)t%6#(91hlzl'
+# SECRET_KEY = 'django-insecure-5eez0p9aa0&wu6^uwj3j-*e(sgjigm8jr2hdnm)t%6#(91hlzl'
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
