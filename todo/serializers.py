0e080930a6d2419c7fb0698e6ae19abb98ba2673
Ajay Nishad <ajaynishad4123@gmail.com>
2026-02-01 16:00:07 +0530

    add create changes

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,8 +1,44 @@
 from rest_framework import serializers
-from .models import Todo
+from .models import Todo, Tag
 
 
 class TodoSerializer(serializers.ModelSerializer):
+    tag_id = serializers.IntegerField(
+        required=False,
+        write_only=True
+    )
+
+    tag = serializers.CharField(
+        source="tag.name",
+        read_only=True
+    )
+
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'is_completed', 'created_at']
+        fields = [
+            "id",
+            "title",
+            "description",
+            "is_completed",
+            "is_archived",
+            "due_date",
+            "tag",
+            "tag_id",
+            "created_at"
+        ]
+
+    def create(self, validated_data):
+        tag_id = validated_data.pop("tag_id", None)
+
+        todo = Todo(**validated_data)
+
+        if tag_id:
+            try:
+                todo.tag = Tag.objects.get(id=tag_id)  # ✅ FIX HERE
+            except Tag.DoesNotExist:
+                raise serializers.ValidationError(
+                    {"tag_id": "Invalid tag"}
+                )
+
+        todo.save()
+        return todo
